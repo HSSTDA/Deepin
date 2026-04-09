2e680fcc1de534e920da06d7bfc2cc9f8b7164d3
hamad <hamadstudyacc@gmail.com>
2026-04-09 16:39:42 +0300

    Initial Commit

---
+++ Deepin/DeepinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepinApp.swift
+//  Deepin
+//
+//  Created by Hamad Abdulaziz on 09/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct DeepinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
